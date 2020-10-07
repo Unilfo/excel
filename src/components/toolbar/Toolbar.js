782e070f806eec3443f6b4d95fd968f0e0e95b4b
Unilfo <kokopal13@gmail.com>
2020-10-07 15:50:34 +0300

    добавил состояния кнопок

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,9 +1,10 @@
-import {ExcelComponent} from '@core/ExcelComponent'
 import {createToolbar} from '@/components/toolbar/toolbar.template'
 import {$} from '@core/dom'
+import {ExcelStateComponent} from '@core/ExcelStateComponent'
+import {defaultStyles} from '@/constanst'
 
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
     static className = 'excel__toolbar'
 
     constructor($root, options) {
@@ -14,14 +15,26 @@ export class Toolbar extends ExcelComponent {
         });
     }
 
+    prepare() {
+        this.initState(defaultStyles)
+    }
+
+    get template() {
+        return createToolbar(this.state)
+    }
+
     toHTML() {
-        return createToolbar()
+        return this.template
     }
 
     onClick(event) {
         const $target = $(event.target)
         if ($target.data.type === 'button') {
-            console.log($target.data.value)
+            const value = JSON.parse($target.data.value)
+            this.$emit('toolbar:applyStyle', value)
+
+            const key =  Object.keys(value)[0]
+            this.setState({[key]: value[key]})
         }
     }
 }
